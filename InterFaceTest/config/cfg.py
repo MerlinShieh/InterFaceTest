361c7dfa507b7dc8b10642828944a6da1262346d
Merlin <shiehmerlin@gmail.com>
2020-06-27 09:40:14 +0800

    将log保存路径放在report一级路径下

--- InterFaceTest/config/cfg.py
+++ InterFaceTest/config/cfg.py
@@ -5,11 +5,11 @@ testCaseExcel_path = os.getcwd()+'/testcase/testCase.xlsx'
 report_path = '/report/TestReport_{}.html'.format(str(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())))
 # report_path = '/report/TestReport.html'
 log_path = {
-	'log_info_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/log/info.log',
-	'log_debug_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/log/debug.log',
-	'log_warn_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/log/warning.log',
-	'log_error_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/log/error.log',
-	'log_critical_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/log/critical.log'
+	'log_info_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/info.log',
+	'log_debug_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/debug.log',
+	'log_warn_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/warning.log',
+	'log_error_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/error.log',
+	'log_critical_path': os.path.dirname(os.getcwd())+'/InterFaceTest/report/critical.log'
 }
 if __name__ == "__main__":
 	print(report_path)
