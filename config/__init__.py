985c9732fa13e944c2c9a0016278a837c6e557d8
MerlinRedmi <merlinshieh@outlook.com>
2023-03-25 00:14:06 +0800

    偷了点代码

--- config/__init__.py
+++ config/__init__.py
@@ -1,11 +1,16 @@
 import os
 import sys
-from units.readYaml import read_yaml_data
+from utils.readYaml import read_yaml_data
 # 项目根目录
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
 # 获取配置文件路径
-API_CONFIG = os.path.join(BASE_DIR, 'config/api_config.ini')
-RUN_CONFIG = os.path.join(BASE_DIR, 'config/run_config.ini')
-DB_CONFIG = os.path.join(BASE_DIR, 'config/db_config.ini')
+API_CONFIG = os.path.join(BASE_DIR, 'config/apiConfig.yaml')
+RUN_CONFIG = os.path.join(BASE_DIR, 'config/runConfig.yaml')
+DB_CONFIG = os.path.join(BASE_DIR, 'config/dbConfig.yaml')
+
+# 获取运行配置信息
+RC = read_yaml_data(RUN_CONFIG)
+INTERVAL = RC['interval']
+PROJECT_NAME = RC['project_name']
