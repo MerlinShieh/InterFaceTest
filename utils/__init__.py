f934974ba5470bf06c98290d8fe0144f560c2c76
MerlinRedmi <merlinshieh@outlook.com>
2023-04-05 21:14:35 +0800

    修改log的来源

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,9 @@
 import sys
 import os
-from utils import new_log
+from utils import _log
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-log = new_log.log
-logger = new_log.logger
+log = _log.log
+logger = _log.logger
