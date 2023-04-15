8fecf9ef45f27e1390a2e2b7ff065654c05bc9e1
MerlinRedmi <merlinshieh@outlook.com>
2023-04-15 16:03:20 +0800

    修改apiSend一个mian的运行问题

--- config/__init__.py
+++ config/__init__.py
@@ -4,6 +4,7 @@ import os
 import sys
 import random
 from utils.readYaml import read_yaml_data
+
 # 项目根目录
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
@@ -14,13 +15,12 @@ RUN_CONFIG = os.path.join(BASE_DIR, 'config/runConfig.yaml')
 DB_CONFIG = os.path.join(BASE_DIR, 'config/dbConfig.yaml')
 
 # 获取运行配置信息
-RC = read_yaml_data(RUN_CONFIG)
+RC = {'interval': 1, "project_name": 1}
 INTERVAL = RC['interval']
 PROJECT_NAME = RC['project_name']
 
 # 虚假请求头
 FAKE_HEADERS = random.choice(json.loads(open(os.path.join(BASE_DIR, 'config/fakeHeaders.json'), 'r').read())['Headers'])
-
 
 if __name__ == '__main__':
     print(FAKE_HEADERS)
