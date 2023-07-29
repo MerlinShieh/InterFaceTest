434a975167c977d1075822af9515e89ea011bcb0
MerlinRedmi <merlinshieh@outlook.com>
2023-07-29 12:16:15 +0800

    接口用例调试，目前断言这块无法做到yaml集合

--- testCase/test_0001_HTTPMethods/test_HTTPMethods.py
+++ testCase/test_0001_HTTPMethods/test_HTTPMethods.py
@@ -2,29 +2,29 @@ import json
 
 import os
 import pytest
-import readFile
-from utils import apiSend, log, logger, BASE_DIR
+from utils import apiSend, log, logger, BASE_DIR, readWriteYmal
 
-yaml_f = readFile.readYaml(os.path.join(BASE_DIR, 'testCase', 'test_0001_HTTPMethods', 'test_HTTPMethods.yaml'))
-'''
-{'feature': 'HTTP 方法', 'story': 'POST方式', 'title': 'POST使用json参数', 
-    'request': 
-        {'method': 'post', 'url': 'https://echo.apifox.com', 'path': '/post', 
-        'headers': {'Content-Type': 'application/json'}, 'mime_type': 'application/json', 
-        'json': 
-            {'username': 'PostMethod', 'passwd': 'json'}, 'data': None, 'params': None, 'file': None, 'cookies': None, 'timeout': 1}, 
-    'validate': None}
-        '''
+os.environ['TOKEN'] = 'token123'
+yaml_f = readWriteYmal.read_yaml_data(os.path.join(BASE_DIR, 'testCase',
+                                                   'test_0001_HTTPMethods', 'test_HTTPMethods.yaml'),
+                                      TOKEN=os.environ['TOKEN'])
+'''{'feature': 'HTTP 方法', 'story': 'POST方式', 'title': 'POST使用json参数', 'request': {'method': 'post', 
+'url': 'https://echo.apifox.com', 'path': '/post', 'headers': {'Content-Type': 'application/json'}, 'mime_type': 
+'application/json', 'json': {'username': 'PostMethod', 'passwd': 'json'}, 'data': None, 'params': None, 'file': None, 
+'cookies': None, 'timeout': 1}, 'validate': ${TOKEN}}'''
 
 
 class TestHTTP:
     @pytest.mark.parametrize('caseinfo', yaml_f)
     @logger(__name__)
     def test_http(self, caseinfo):
-        log.debug(f'开始运行用例 {caseinfo}')
+        log.debug(f'开始运行用例 \n {caseinfo}')
         resp = apiSend.send_request(caseinfo)
         log.debug('code: {}; data: {}', resp[0], resp[1])
+        log.info('断言: ↓↓↓↓↓↓↓↓↓↓↓')
+        log.debug(caseinfo['validate'])
+        assert resp[0] == 200
 
 
 if __name__ == '__main__':
-    print(yaml_f)
+    pytest.main()
